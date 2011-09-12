c4c1bea822d50634d6fc8c1ad8b2dccd78ab03d9
Ivan Morgillo <imorgillo@gmail.com>
2011-09-12 23:00:28 +0200

    Fix DroniX settings menu item
    DEM is out of Settings now. We just call an external
    PreferenceActivity located in DEM:
    org.dronix.android.dronixextendedmenu.Preferences
    
    Change-Id: I75ef57b0b73a34befe61969611882f9073f463f6

--- Android.mk
+++ Android.mk
@@ -3,19 +3,12 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := libroottools
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
-##################################################
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libroottools:RootTools-1.1.0-sdk3-generic.jar
-include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
